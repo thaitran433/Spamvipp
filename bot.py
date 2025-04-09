e0b54c205e36051d3fc64b6cc40e6307295c5c41
huydoan6339999 <ngocq8692@gmail.com>
2025-04-09 22:56:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import tempfile
 import subprocess
 from keep_alive import keep_alive
 
-TOKEN = "8025225779:AAHjpXQ9OZ3aoiv1pQn_tFQHezKDtza0Rgo"
+TOKEN = "8025225779:6320148381:AAFKjSSsm7GM0WURbYNnNsbwi7crogOYzuo"
 bot = telebot.TeleBot(TOKEN)
 
-name_bot = "SpamVip"
+name_bot = "Bảo Huy"
 
 @bot.message_handler(commands=['spam'])
 def spam(message):
